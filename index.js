9fb74d9fd3d9e7c38b9d07cadaf7367ddc33bb8c
TalebZhang <158696190+TalebZhang@users.noreply.github.com>
2024-02-05 10:42:47 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,148 @@
+document.getElementById("myBtn").addEventListener("click", divHide);
+document.getElementById("myBtn1").addEventListener("click", divHide1);
+document.getElementById("myBtn2").addEventListener("click", divHide2);
+document.getElementById("myBtn3").addEventListener("click", divHide3);
+document.getElementById("myBtn4").addEventListener("click", divHide4);
+document.getElementById("myBtn5").addEventListener("click", divHide5);
+document.getElementById("myBtn6").addEventListener("click", divHide6);
+document.getElementById("myBtn7").addEventListener("click", divHide7);
+document.getElementById("myBtn8").addEventListener("click", divHide8);
+document.getElementById("english").addEventListener("click", divHide9);
+document.getElementById("chinese").addEventListener("click", divHide10);
+
+function divHide() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "block";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+}
+
+function divHide1() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+	
+	
+}
+
+function divHide2() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+	
+}
+
+function divHide3() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "block";
+	document.querySelector(".healthadvice").style.display = "none";
+	
+}
+
+function divHide4() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "block";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+	
+}
+
+function divHide5() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+
+	
+}
+
+function divHide6() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+	
+}
+
+function divHide7() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+	
+}
+
+function divHide8() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+	
+}
+
+function divHide9() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "none";
+	}
+	document.querySelector(".englishreading").style.display = "block";
+	document.querySelector(".myreading").style.display = "none";
+	document.querySelector(".myexperience").style.display = "none";
+	document.querySelector(".healthadvice").style.display = "none";
+
+}
+
+function divHide10() {
+	const nodeList = document.querySelectorAll(".englishlearning");
+	for (let i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.display = "block";
+	}
+	document.querySelector(".englishreading").style.display = "none";
+	document.querySelector(".myreading").style.display = "block";
+	document.querySelector(".myexperience").style.display = "block";
+	document.querySelector(".healthadvice").style.display = "block";
+}
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
