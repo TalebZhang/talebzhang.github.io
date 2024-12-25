5d5b40b05a5d55ae70e720578be48f359f1c1b76
TalebZhang <158696190+TalebZhang@users.noreply.github.com>
2024-12-25 10:20:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -317,3 +317,7 @@ function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+if(window.location.href.endsWith(".html")) {
+  window.location.href = window.location.href.replace(".html", "");  
+}
